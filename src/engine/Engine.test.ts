cb5969020c1fbaa25a74ba3e400c3f819086f0cd
Miles Olson <mpolson64@gmail.com>
2020-05-17 12:54:38 -0700

    boid movement

--- src/engine/Engine.test.ts
+++ src/engine/Engine.test.ts
@@ -1,16 +1,8 @@
 import { next } from './Engine'
 
-test('next', () => {
-    const position: [number, number] = [0, 0];
-    const velocity: [number, number] = [0, 0];
-
+test('next on empty does not mutate', () => {
     const world = {
-        boids: [
-            {
-                position,
-                velocity,
-            }
-        ],
+        boids: [],
         lookAngle: 0,
         lookRadius: 0,
     };
@@ -21,4 +13,34 @@ test('next', () => {
     expect(nextWorld.boids.length).toEqual(world.boids.length);
     expect(nextWorld.lookAngle).toEqual(world.lookAngle);
     expect(nextWorld.lookRadius).toEqual(world.lookRadius);
+});
+
+
+test('next on single boid', () => {
+    const position: [number, number] = [0, 0];
+    const velocity: [number, number] = [1, 0];
+    const boid = {
+        position,
+        velocity,
+    };
+
+    const world = {
+        boids: [boid],
+        lookAngle: 0,
+        lookRadius: 0,
+    };
+
+    const nextWorld = next(world);
+    const nextNextWorld = next(nextWorld);
+    const nextBoid = nextWorld.boids[0];
+    const nextNextBoid = nextNextWorld.boids[0];
+
+    expect(boid.velocity).toEqual([1, 0]);  // velocity should not change
+    expect(boid.position).toEqual([0, 0]);  // should move 1 to the right
+
+    expect(nextBoid.velocity).toEqual([1, 0]);  // velocity should not change
+    expect(nextBoid.position).toEqual([1, 0]);  // should move 1 to the right
+
+    expect(nextNextBoid.velocity).toEqual([1, 0]);  // velocity should not change
+    expect(nextNextBoid.position).toEqual([2, 0]);  // should move 1 to the right
 });
--- src/engine/Engine.ts
+++ src/engine/Engine.ts
@@ -2,11 +2,59 @@ import { Boid } from '../types/Boid'
 import { World } from '../types/World'
 
 export function next(world: World): World {
-    const nextBoids = world.boids.map(boid => boid)
+    const nextBoids = world.boids.map(boid => {
+        const visible = filterVisible(world.lookAngle, world.lookRadius, boid.position, world.boids);
+
+        const separated = separate(visible, boid);
+        const aligned = align(visible, boid);
+        const cohered = cohere(visible, boid);
+
+        const velocity: [number, number] = [
+            (separated[0] + aligned[0] + cohered[0]) / 3,
+            (separated[1] + aligned[1] + cohered[1]) / 3,
+        ];
+
+        const position: [number, number] = [
+            boid.position[0] + velocity[0],
+            boid.position[1] + velocity[1],
+        ];
+
+        return {
+            position,
+            velocity,
+        };
+    });
 
     return {
         boids: nextBoids,
         lookAngle: world.lookAngle,
         lookRadius: world.lookRadius
     };
+}
+
+// TODO
+function filterVisible(lookAngle: number, lookRadius: number, position: [number, number], boids: Boid[]): Boid[] {
+    return boids.filter(
+        boid => {
+            const inRadius = true;
+            const inAngle = true;
+
+            return inRadius && inAngle;
+        }
+    );
+}
+
+// TODO
+function separate(visible: Boid[], boid: Boid): [number, number] {
+    return boid.velocity;
+}
+
+// TODO
+function align(visible: Boid[], boid: Boid): [number, number] {
+    return boid.velocity;
+}
+
+// TODO
+function cohere(visible: Boid[], boid: Boid): [number, number] {
+    return boid.velocity;
 }
